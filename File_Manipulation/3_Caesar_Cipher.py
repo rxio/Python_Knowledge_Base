ef8ba5f519de707954442bf7189cb855f43eab2e
Richard <xiongrichard@gmail.com>
2017-08-23 14:21:24 -0400

    20170823

---
+++ File_Manipulation/3_Caesar_Cipher.py
@@ -0,0 +1,16 @@
+key = 5
+
+f = open("encoded.txt", "w")
+
+while True:
+	char = input("Enter a letter: ")
+
+	if len(char) != 1:
+		break
+
+	code = chr(ord(char)+key)
+	print(code)
+
+	f.write(code + "\n")
+
+f.close()
