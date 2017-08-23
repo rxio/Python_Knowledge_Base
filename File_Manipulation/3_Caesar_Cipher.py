504b7889aa7f13e2e8858d630834abe4b488d22b
Richard <xiongrichard@gmail.com>
2017-08-23 15:19:50 -0400

    20170823

--- File_Manipulation/3_Caesar_Cipher.py
+++ File_Manipulation/3_Caesar_Cipher.py
@@ -1,9 +1,14 @@
+#! /usr/bin/python3
+
 key = 5
 
 f = open("encoded.txt", "w")
 
 while True:
-	char = input("Enter a letter: ")
+	try:
+		char = input("Enter a letter: ")
+	except:
+		print("a")
 
 	if len(char) != 1:
 		break
