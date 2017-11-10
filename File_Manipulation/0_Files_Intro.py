7f3f7356f549031d5cf3bc49e02db092b4d74925
Richard <xiongrichard@gmail.com>
2017-11-09 19:49:47 -0500

    20171109

--- File_Manipulation/0_Files_Intro.py
+++ File_Manipulation/0_Files_Intro.py
@@ -1,7 +1,8 @@
-file = open("numbers.txt", "r") #If you don't pass in a second argument, it defaults to "r"
+file = open("numbers.txt", "r") #This line returns the file handle in read mode and stores it in the variable named 'file'
+#If you don't pass in a second argument, it defaults to "r"
 
 print(file) #This prints the file handle
 print(file.name) #This prints the name of the file
 print(file.mode) #This prints the mode that the file is currently in
 
-file.close()
+file.close() #This closes the file handle ?
--- File_Manipulation/1_File_Intro.py
+++ File_Manipulation/1_File_Intro.py
@@ -1,7 +1,7 @@
 file = open("example.txt", "w")
 
 for i in range (21):
-	file.write(str(i) + "\n")
+	file.write(str(i) + "\n") #You have to use the str() function because you are concatenating it to the string "\n", and you can't add ints to strings. If it was just "i" alone, you wouldn't have to use str() to convert it to a string
 
 file.close()
 
@@ -9,6 +9,6 @@ file.close()
 filein = open("example.txt", "r")
 
 for i in filein:
-	print(i.rstrip())
+	print(i.rstrip()) #The reason why I use rstrip() is because print() adds another \n to the end of the line
 
 filein.close()
